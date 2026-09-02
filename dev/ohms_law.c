53b9c0c47c92a287ff9192ffe8a1fdf78a1ad107
Documenter.jl <documenter@juliadocs.github.io>
2026-09-02 18:23:23 +0000

    build based on 8485cfb

--- dev/ohms_law.c
+++ dev/ohms_law.c
@@ -1,5 +1,5 @@
 #include <math.h>
 void ohms_law_evaluator(double* du, const double RHS1, const double RHS2, const double RHS3, const double RHS4) {
   du[0] = RHS1 / RHS2;
-  du[1] = sqrt(pow(1 / RHS2, 2) * (RHS3 * RHS3) + pow((-1 * RHS1 * 1) / (RHS2 * RHS2), 2) * (RHS4 * RHS4));
+  du[1] = hypot((-1 * RHS1 * RHS4) / (RHS2 * RHS2), RHS3 / RHS2);
 }
